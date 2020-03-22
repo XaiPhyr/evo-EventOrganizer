5224403ab5488443ec20ba2514e75e0b3616650a
xaiphyr <arcadia.initiative@gmail.com>
2020-03-22 23:08:36 +0800

    debugging

--- views/pages/dashboard/index.php
+++ views/pages/dashboard/index.php
@@ -3,8 +3,7 @@ include("dashboard.module.php");
 ?>
 
 <div class="container col-11">
-    <h1>Hello World</h1>
-    <!-- <div class="row">
+    <div class="row">
         <div class="col-12">
             <?php include("_announcements.php"); ?>
         </div>
@@ -23,5 +22,5 @@ include("dashboard.module.php");
         <div class="nav mb-5 justify-content-center mx-auto">
             <a style="font-size: .875rem" href="?page=eventlists" class="text-decoration-none">Show more...</a>
         </div>
-    <?php } ?> -->
+    <?php } ?>
 </div>
