06ff873f1ca12a50f7bc64e7b85e811d90e17759
XaiPhyr <arcadia.initiative@gmail.com>
2020-03-22 22:53:23 +0800

    Update index.php

--- views/pages/dashboard/index.php
+++ views/pages/dashboard/index.php
@@ -3,6 +3,8 @@ include("dashboard.module.php");
 ?>
 
 <div class="container col-11">
+    
+    <h1>Hello WORLD </h1>
     <div class="row">
         <div class="col-12">
             <?php include("_announcements.php"); ?>
